16c04e7a951609344dd9ad1044e8b62e57de1767
Marcos Alves Oliveira <malvesol@emeal.nttdata.com>
2022-01-25 15:15:48 -0300

    commit inicial

---
+++ src/main/java/com/marcos/sistemaCurso/SistemaCursoApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.sistemaCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCursoApplication.class, args);
+	}
+
+}
